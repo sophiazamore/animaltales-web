c3e7020035dd8c3fbdd05e5826d3f3169ba8288b
John Carlo Miel <johncarlomiel@gmail.com>
2025-09-07 00:09:47 +0800

    fix incorrect path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/animaltales-web/',
   plugins: [react(), tailwindcss()],
 })
